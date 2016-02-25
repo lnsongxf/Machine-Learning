d61104d04c71f6779434ec405c2902170b85b8b7
Ying-Ting Lin <claire4621@gmail.com>
2016-02-24 20:00:59 -0600

    Update naivebayes.m

--- Project-4/naivebayes.m
+++ Project-4/naivebayes.m
@@ -1,1 +1,20 @@
+function logratio = naivebayes(x,y,x1)
+% function possi = naivebayes(x,y);
+%
+% Computation of log P(Y|X=x1) using Bayes Rule
+% Input:
+% x : n input vectors of d dimensions (dxn)
+% y : n labels (-1 or +1)
+% x1: input vector of d dimensions (dx1)
+%
+% Output:
+% poscond: log conditional probability log P(Y = 1|X=x1)
+% negcond: log conditional probability log P(Y = -1|X=x1)
+%
+
+[d,n] = size(x);
+%% fill in code here
+ [pos,neg] = naivebayesPY(x,y);
+ [posprob,negprob] = naivebayesPXY(x,y);
+ logratio = log(pos*prod(posprob)/neg*prod(negprob))
 
