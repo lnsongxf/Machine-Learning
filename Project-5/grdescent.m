9e4393583fc3ede2fca2476480985efe25a4939a
Ying-Ting Lin <claire4621@gmail.com>
2016-03-18 18:51:47 -0500

    Update grdescent.m

--- Project-5/grdescent.m
+++ Project-5/grdescent.m
@@ -1,1 +1,36 @@
+function [w]=grdescent(func,w0,stepsize,maxiter,tolerance)
+% function [w]=grdescent(func,w0,stepsize,maxiter,tolerance)
+%
+% INPUT:
+% func function to minimize
+% w0 = initial weight vector 
+% stepsize = initial gradient descent stepsize 
+% tolerance = if norm(gradient)<tolerance, it quits
+%
+% OUTPUTS:
+% 
+% w = final weight vector
+%
+
+if nargin<5,tolerance=1e-02;end;
+iter = 0;
+gradient =ones(size(w0));
+loss0 = 0;
+w = w0;
+
+while iter <=maxiter && norm(gradient)>tolerance;
+    
+    [loss,gradient]=func(w0);
+    w = w0 -stepsize*gradient;
+    iter = iter +1;
+    w0 = w;
+    if loss<loss0;
+        stepsize = 1.01*stepsize;
+    else
+        stepsize = 0.5*stepsize;
+    end;
+    loss0 = loss;
+
+end;
+
 
