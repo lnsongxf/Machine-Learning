ac56b475eb697f543d9d35e5c2beaaccf3d29b3e
Ying-Ting Lin <claire4621@gmail.com>
2016-04-24 02:50:48 -0500

    Update forest.m

--- Project-8/forest.m
+++ Project-8/forest.m
@@ -1,1 +1,22 @@
+function F=forest(x,y,nt)
+% function F=forest(x,y,nt)
+%
+% INPUT:
+% x | input vectors dxn
+% y | input labels 1xn
+% nt | number of samples
 
+% OUTPUT:
+% F | Forest (The output format should be an array of id3trees.)
+%
+
+F = cell(1,nt);
+[d,n] = size(x);
+for t = 1:nt
+    s = randsample(n, n,true);
+    X = x(:,s); % X is the sample training data;
+    
+    T = id3tree(X,Y);
+    T = prunetree(T,x,y) ;
+    F{1, t} = T; %add the tree to a cell array
+end;
