d848360cb9b3caafc5d0b18d62b38e57f42ab6c0
Ying-Ting Lin <claire4621@gmail.com>
2016-04-27 13:26:32 -0500

    Update evalboost.m

--- Project-8/evalboost.m
+++ Project-8/evalboost.m
@@ -12,5 +12,18 @@ function preds=evalboost(BDT,xTe)
 % preds | predictions of labels for xTe
 %
 
+preds = 0;
+total_weight= 0;
+for i = 1:size(BDT,2)
+    preds = preds+  BDT{1,i}*evaltree(BDT{2,i}, xTe);
+    total_weight = total_weight + BDT{1,i};
+end;
+preds = preds/total_weight;
 
-preds = feval(BDT,xTe);
+for i = 1: size(preds, 2)
+   if preds(1,i) >= 1.45
+       preds(1,i) = 2;
+   else;
+       preds(1,i) = 1;
+   end;
+end
