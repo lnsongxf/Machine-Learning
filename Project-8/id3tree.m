c0a72bef066769f3fd0adfde77fb07eae8062029
Ying-Ting Lin <claire4621@gmail.com>
2016-04-27 23:10:24 -0500

    Update id3tree.m

--- Project-8/id3tree.m
+++ Project-8/id3tree.m
@@ -19,10 +19,10 @@ function T=id3tree(xTr,yTr,maxdepth,weights)
 
 if nargin==3,
     q = 1+ (maxdepth-1)*maxdepth;
-    weights=ones(1,n)./n;
+    weights=ones(1,n)/n;
 elseif nargin==2
     maxdepth = inf;
-    weights=ones(1,n)./n;
+    weights=ones(1,n)/n;
     %set the maximum number of nodes as every leaf has only 1 observation
     q = (1+n)*n/2;
 else %nargin = 4
@@ -32,10 +32,13 @@ end
 S = cell(2,q); 
 T = zeros(6,q);
 next_node = 2; %index of next subtree
+%store the remaining data at each node in cell array S
 S{1,1} = xTr; 
 S{2,1} = yTr;
 
 for i = 1:q
+    %the condition takes place when nextnode does not increase because if
+    %it does, it will exceed the allowed dimension of T
     if i==next_node
         T(:,i:end) = [];
         break;
@@ -50,24 +53,26 @@ for i = 1:q
         T(2:4,i) = [feature;cut;next_node];
         S{1, next_node} = x(:,find(x(feature,:)<=cut));
         S{2, next_node} = y(:,find(x(feature,:)<=cut));
-        if next_node <= q
-            T(6,next_node) = i;
-        end;
+
+        T(6,next_node) = i;
         
         next_node = next_node+1;
         T(5,i) = next_node;
         S{1, next_node} = x(:,find(x(feature,:)>cut));
         S{2, next_node} = y(:,find(x(feature,:)>cut));
-        if next_node <= q
-            T(6,next_node) = i;
-        end;
+        T(6,next_node) = i;
      %if further split would exceed the maximum number of nodes allowed, set the index of left & right subtrees to zero
      %otherwise, move on the the next node
-        if (next_node+1)>q
+        if next_node>q
             T(4:5,i)=0;
         else
             next_node = next_node+1;
         end
     end
 
-end    
+end     
+%the if condition is needed, otherwise, the dimension of T may exceed 
+%that allowed by maxdepth since we have T(6,next_node) = i in the for loop
+if size(T,2)> q
+    T(:, q+1:end) = [];
+end;
