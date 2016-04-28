bcbfcc15b37cc84c6575a02676a5028536e7d147
Ying-Ting Lin <claire4621@gmail.com>
2016-04-27 22:35:39 -0500

    Update id3tree.m

--- Project-8/id3tree.m
+++ Project-8/id3tree.m
@@ -1,1 +1,73 @@
+function T=id3tree(xTr,yTr,maxdepth,weights)
+% function T=id3tree(xTr,yTr,maxdepth,weights)
+%
+% The maximum tree depth is defined by "maxdepth" (maxdepth=2 means one split). 
+% Each example can be weighted with "weights". 
+%
+% Builds an id3 tree
+%
+% Input:
+% xTr | dxn input matrix with n column-vectors of dimensionality d
+% xTe | dxm input matrix with n column-vectors of dimensionality d
+% maxdepth = maximum tree depth
+% weights = 1xn vector where weights(i) is the weight of example i
+% 
+% Output:
+% T = decision tree 
 
+[~,n] = size(xTr);
+
+if nargin==3,
+    q = 1+ (maxdepth-1)*maxdepth;
+    weights=ones(1,n)./n;
+elseif nargin==2
+    maxdepth = inf;
+    weights=ones(1,n)./n;
+    %set the maximum number of nodes as every leaf has only 1 observation
+    q = (1+n)*n/2;
+else %nargin = 4
+    q = 1+ (maxdepth-1)*maxdepth;
+end
+
+S = cell(2,q); 
+T = zeros(6,q);
+next_node = 2; %index of next subtree
+S{1,1} = xTr; 
+S{2,1} = yTr;
+
+for i = 1:q
+    if i==next_node
+        T(:,i:end) = [];
+        break;
+    end
+    x = S{1,i};
+    y = S{2,i};
+    [feature,cut,~] = entropysplit(x,y,weights);
+    T(1,i) = mode(y);
+    %no more subtrees if no better split found or if the node only has one
+    %data point
+    if ~(feature==0 || size(x,2)==1)
+        T(2:4,i) = [feature;cut;next_node];
+        S{1, next_node} = x(:,find(x(feature,:)<=cut));
+        S{2, next_node} = y(:,find(x(feature,:)<=cut));
+        if next_node <= q
+            T(6,next_node) = i;
+        end;
+        
+        next_node = next_node+1;
+        T(5,i) = next_node;
+        S{1, next_node} = x(:,find(x(feature,:)>cut));
+        S{2, next_node} = y(:,find(x(feature,:)>cut));
+        if next_node <= q
+            T(6,next_node) = i;
+        end;
+     %if further split would exceed the maximum number of nodes allowed, set the index of left & right subtrees to zero
+     %otherwise, move on the the next node
+        if (next_node+1)>q
+            T(4:5,i)=0;
+        else
+            next_node = next_node+1;
+        end
+    end
+
+end    
